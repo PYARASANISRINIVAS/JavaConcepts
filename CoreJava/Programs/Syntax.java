8138183753020d1011744d614ca9315b81a03819
PYARASANISRINIVAS <208r1a05m4@gmail.com>
2023-07-01 13:14:26 +0530

    my java course

---
+++ CoreJava/Programs/Syntax.java
@@ -0,0 +1,9 @@
+package CoreJava.Programs;
+
+public class Syntax { // class Definiation
+    public static void main(String[] args){ //main method.
+        System.out.println("Hello World"); //print statement.
+
+    }
+}
+
